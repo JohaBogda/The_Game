ca65896790d30ba3e5ee7a8a3bf3b2d02ce834b0
unknown <bogdanowj@gmail.com>
2021-08-13 23:46:37 -0400

    got the function for random computer numbers to work

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
+const rock = 1;
+const paper = 2;
+const scissors = 3;
+
+
 function playerTurn() {
+
     console.log('hey we clicked a button with a value X')
     // do logic here, save value in a variable
     
     // make sure you call this at the end after all your playerTurn logic
-    computerTurn()
+    computerTurn(3)
   }
   
-  function computerTurn() {
-    //logic to make computer do something randomly
-    console.log('hey i chose something randomly')
+  function computerTurn(max) {
+
+     if(Math.floor(Math.random()*max) === 1){
+          console.log("computer chose rock")
+      }
+
+      else if(Math.floor(Math.random()*max) === 2){
+              console.log("computer chose paper")
+        }
+
+        else{
+            console.log("computer chose scissors")
+        }
   }
   
   // when we click a button for player to choose thier value, we would call the F playerTurn
   playerTurn()
+  
