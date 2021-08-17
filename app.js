3f592e305fc835d2ab56ee75a1d795bed1c69ce5
unknown <bogdanowj@gmail.com>
2021-08-17 11:47:52 -0400

    Tory helped with prompt to insert name into the website + put images within the buttons

--- app.js
+++ app.js
@@ -1,31 +1,50 @@
-const playerScore = document.querySelector("playerScore")
-const robotScore = document.querySelector("robotScore")
+const playerScore = document.querySelector("#playerScore")
+const earthScore = document.querySelector("#earthScore")
+
+let playerName = document.querySelector(".playerName")
+function playerNameSelect(){
+  var newPlayer =  prompt("What's your name?")
+  console.log(newPlayer)
+  playerName.innerHTML = newPlayer
+}
+playerNameSelect()
+
+// while i for either players is <=3 (they need to win 3 out of 5) continue playing
+// 
+// player selects either fire, wind, or water
+
+// when player selects fire, run computerTurn to have computer select an element; 
+//      if computer selects water, #earthScore ++
 
 
-function playerTurn() {
+function playerTurn() { // each time player selects a button
+    while(i =0, i<=3, i++){ // while player hasn't won 3 out of 5 games 
+        function selectFire() { //why is this function not declared?
+            computerTurn()
+            if(computerTurn === 1){
+                console.log(playerScore +1) // connect back to playerScore so it goes up a point if won
+            }
+            
 
 
-
-    console.log('hey we clicked a button with a value X')
-    // do logic here, save value in a variable
-    
-    // make sure you call this at the end after all your playerTurn logic
-    computerTurn(3)
-  }
-  
-  function computerTurn(max) {
-
-     if(Math.floor(Math.random()*max) === 1){
-          console.log("computer chose rock")
-      }
-      else if(Math.floor(Math.random()*max) === 2){
-              console.log("computer chose paper")
         }
-        else{
-            console.log("computer chose scissors")
-        }
-  }
+    }
+}
+playerTurn()
+
+function computerTurn() {
+    if(Math.floor(Math.random()*3) === 1){
+         //console.log("computer chose fire")
+     }
+     else if(Math.floor(Math.random()*3) === 2){
+             //console.log("computer chose wind")
+       }
+       else{
+           //console.log("computer chose water")
+       }
+ }
+ //computerTurn() //put inside of playerTurn function to run each time player clicks a button
   
-  // when we click a button for player to choose thier value, we would call the F playerTurn
-  playerTurn()
+
+
   
