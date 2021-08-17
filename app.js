a6d2afdd241c32b765d54c92198f6ec95631f411
unknown <bogdanowj@gmail.com>
2021-08-17 14:48:57 -0400

    added event listeners to buttons

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const playerScore = document.querySelector("#playerScore")
 const earthScore = document.querySelector("#earthScore")
 
-const fireButton = document.querySelector("fire")
-const windButton = document.querySelector("wind")
-const waterButton = document.querySelector("water")
+const fireButton = document.querySelector("#fire")
+const windButton = document.querySelector("#wind")
+const waterButton = document.querySelector("#water")
 
-let playerName = document.querySelector(".playerName")
-function playerNameSelect(){
-  var newPlayer =  prompt("What's your name?")
+let playerName = document.querySelector(".playerName") // make variable to grab player name
+function playerNameSelect(){ // create a function for player to input their name 
+  let newPlayer =  prompt("What's your name?") // create variable for prompt
   console.log(newPlayer)
-  playerName.innerHTML = newPlayer
+  playerName.innerHTML = newPlayer // have whatever is input into the prompt show up in the playerName spot 
 }
 playerNameSelect()
 
@@ -34,22 +34,32 @@ playerNameSelect()
 
 // GOAL: once either score = 3 the person wins/ ends the loop
 
+fireButton.addEventListener("click", function(){
+    console.log("Player clicked fire");
+})
 
+windButton.addEventListener("click", function(){
+    console.log("Player clicked wind");
+})
 
-function playerTurn() { // each time player selects a button
-    while(i =0, i<=3, i++){ // while player hasn't won 3 out of 5 games 
-        function selectFire() { //why is this function not declared?
-            computerTurn()
-            if(computerTurn === 1){
-                console.log(playerScore +1) // connect back to playerScore so it goes up a point if won
-            }
+waterButton.addEventListener("click", function(){
+    console.log("Player clicked water");
+})
+
+// function playerTurn() { // each time player selects a button
+//     while(i =0, i<=3, i++){ // while player hasn't won 3 out of 5 games 
+//         function selectFire() { //why is this function not declared?
+//             computerTurn()
+//             if(computerTurn === 1){
+//                 console.log(playerScore +1) // connect back to playerScore so it goes up a point if won
+//             }
             
 
 
-        }
-    }
-}
-playerTurn()
+//         }
+//     }
+// }
+// playerTurn()
 
 function computerTurn() {
     if(Math.floor(Math.random()*3) === 1){
@@ -63,7 +73,4 @@ function computerTurn() {
        }
  }
  //computerTurn() //put inside of playerTurn function to run each time player clicks a button
-  
-
-
   
